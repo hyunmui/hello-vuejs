60345a07dee86d3e3de154426a3831c2c03490ac
Hyeonmin Seon <hyunmui@outlook.kr>
2020-11-30 23:07:04 +0900

    4장 완료

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <ul>
+    <?php for ($i = 0; $i < 12; $i++) : ?>
+      <li><?= $i ?></li>
+    <?php endfor; ?>
+  </ul>
+</body>
+
+</html>
